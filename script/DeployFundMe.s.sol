758aeca6a29e706f70e53aece27fc482e44d7422
Rajiv620019 <rajiv620019@gmail.com>
2023-08-03 12:30:23 +0530

    Refacrtored code & passed pricefeed to constructor

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -6,9 +6,10 @@ import {FundMe} from "../src/FundMe.sol";
 
 contract DeployFundMe is Script {
     // Deployed FundMe contract
-    function run() external {
+    function run() external returns (FundMe) {
         vm.startBroadcast();
-        new FundMe();
+        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         vm.stopBroadcast();
+        return fundMe;
     }
 }
--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -3,12 +3,14 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
-        fundMe = new FundMe();
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     // Testing min USD is $5
@@ -19,6 +21,7 @@ contract FundMeTest is Test {
     // Testing price feed version
     function testPriceFeedVersion() public {
         uint256 version = fundMe.getVersion();
+        console.log(version);
         assertEq(version, 4);
     }
 }
