7705a8e08966e92709fffa201f881c4bc086d5b5
Rajiv620019 <rajiv620019@gmail.com>
2023-08-03 20:53:05 +0530

    Implemented anvilConfig

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,9 +3,13 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
-contract HelperConfig {
+contract HelperConfig is Script {
     NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_ANSWER = 2000e8;
 
     struct NetworkConfig {
         address priceFeed;
@@ -27,5 +31,17 @@ contract HelperConfig {
     }
 
     // Config to get Mock anvil
-    function getAnvilConfig() public pure returns (NetworkConfig memory) {}
+    function getAnvilConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        vm.startBroadcast();
+
+        MockV3Aggregator mockV3Aggregator = new MockV3Aggregator(DECIMALS, INITIAL_ANSWER);
+        vm.startBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({priceFeed: address(mockV3Aggregator)});
+        return anvilConfig;
+    }
 }
