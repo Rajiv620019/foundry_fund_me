cca55c8e25b56dc6232dcbfa47b9d268673c28a1
Rajiv620019 <rajiv620019@gmail.com>
2023-08-05 12:24:05 +0530

    Wrote tests for interactions

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -2,11 +2,29 @@
 
 pragma solidity ^0.8.18;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+import {FundMe} from "../src/FundMe.sol";
 
 contract FundFundMe is Script {
-    function run() external {}
+
+    uint256 constant SEND_VALUE = 0.1 ether;
+
+    function fundFundMe(address mostRecentDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable (mostRecentDeployed
+        )).fund{value: SEND_VALUE}();
+        vm.stopBroadcast();
+        console.log("Funded with %s", SEND_VALUE);
+
+    }
+
+    function run() external {
+
+        address mostRecentDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+
+        fundFundMe(mostRecentDeployed);    
+    }
 }
 
 contract WithdrawFundMe is Script {}
---
+++ test/integration/Interactions.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+
+contract Interactions is Test {
+    FundMe fundMe;
+    HelperConfig public helperConfig;
+
+    address USER = makeAddr("rajiv");
+    uint256 constant AMOUNT = 0.1 ether;
+    uint256 constant BALANCE = 10 ether;
+
+    function setUp() external {
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+        vm.deal(USER, BALANCE);
+    }
+}
